b8ea7d55d932d65e6ba5c3b298c6594f0807e1ff
Muhammad Nazirul Asyraaf Bin Halid <mn.asyraaf@graduate.utm.my>
2024-09-29 11:12:56 +0800

    add typescript

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 import autoprefixer from 'autoprefixer'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(() => {
   return {
@@ -12,6 +13,7 @@ export default defineConfig(() => {
     css: {
       postcss: {
         plugins: [
+          tsconfigPaths(),
           autoprefixer({}), // add options if needed
         ],
       },
